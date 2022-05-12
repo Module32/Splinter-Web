c365401654d6d340c0722c4b0b6ec9f931cb187e
Module32 <derivativeco2021@gmail.com>
2022-05-12 01:41:52 +0000

    updates

--- splinter-web/pages/index.js
+++ splinter-web/pages/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+77import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 import Layout from '../components/layout'
@@ -19,13 +19,25 @@ export default function Home() {
             <h1 style={{fontWeight: 700, fontSize: '4.5vw', margin: 0}}>The Discord bot that can <span className="cursive" style={{color: "#8b87ff"}}>do more</span>.</h1>
             <h2 style={{fontWeight: '600'}}>Splinter is a highly capable multipurpose bot with a huge list of features, so you only need one bot for all your servers!<br /><br /><a href="https://discord.com/oauth2/authorize?client_id=920207039043625030&scope=bot%20applications.commands" className="thick">Invite Now</a></h2>
           </div>
-          <div style={{textAlign: 'center', paddingTop: '20px'}}>
+          <div style={{textAlign: 'center', paddingTop: '50px'}}>
             <h1><span style={{color: "#ffab19"}}><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span> Rated <a href="https://top.gg/bot/920207039043625030">5/5 on Top.gg</a></h1>
-          <p>Learn more about some of the features that our users love!</p>
+          <h2 className="grey">Learn more about some of the features that our users love!</h2>
             <div style={{display: 'flex', justifyContent: 'center'}}>
               <div className="card">
-                <h2>Hi</h2>
+                <h2 style={{color: '#29d1ff'}}>🎊 Giveaways</h2>
+                <h3 className="grey">gstart<br />reroll<br />messages<br />bonusentries<br /></h3>
               </div>
+
+              <div className="card">
+                <h2 style={{color: '#29d1ff'}}>🔨 Moderation</h2>
+                <h3 className="grey">kick<br />ban<br />mute<br />unmute<br />And more!</h3>
+              </div>
+
+              <div className="card">
+                <h2 style={{color: '#29d1ff'}}>🤣 Fun</h2>
+                <h3 className="grey">susrate<br />testiq<br />poggers<br />damnbrothatscrazy<br />And more!</h3>
+              </div>
+              
             </div>
           </div>
           <div className="hometop">
@@ -35,7 +47,7 @@ export default function Home() {
                 width="1028"
                 height="762"
                 />
-              <h1><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>🥳 Extensive Giveaway System</span><br /><p style={{fontSize: '18px', fontWeight: '600'}}>Splinter&apos;s giveaway module is by far the most capable one on Discord. With various features such as rerolling, adding bonus entires, message requirements, and more!</p></h1>
+              <h1><p style={{fontSize: '14px', margin: '0', padding: '0'}} className="grey">G I V E A W A Y S</p><br /><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>🥳 Extensive Giveaway System</span><br /><p style={{fontSize: '21px', fontWeight: '600'}}>Splinter&apos;s giveaway module is by far the most capable one on Discord. With various features such as rerolling, adding bonus entires, message requirements, and more!</p></h1>
             </div>
             
             <div className="infocard">
@@ -44,7 +56,7 @@ export default function Home() {
                 width="1252"
                 height="788"
                 />
-              <h1><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>🥾 Powerful Moderation Tools</span><br /><p style={{fontSize: '18px', fontWeight: '600'}}>Level up your server&apos;s moderation tools with Splinter. A wide variety of commands like <span className="tag">kick</span>, <span className="tag">mute</span>, <span className="tag">purge</span>, and more all help you moderate your server with efficiency and ease!</p></h1>
+              <h1><p style={{fontSize: '14px', margin: '0', padding: '0'}} className="grey">M O D E R A T I O N + A D M I N</p><br /><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>🥾 Powerful Moderation Tools</span><br /><p style={{fontSize: '21px', fontWeight: '600'}}>Level up your server&apos;s moderation tools with Splinter. A wide variety of commands like <span className="tag">kick</span>, <span className="tag">mute</span>, <span className="tag">purge</span>, and more all help you moderate your server with efficiency and ease!</p></h1>
             </div>
 
             <div className="infocard">
@@ -53,8 +65,21 @@ export default function Home() {
                 width="590"
                 height="778"
                 />
-              <h1><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>😜 All Things Fun!</span><br /><p style={{fontSize: '18px', fontWeight: '600'}}>Looking to bring some fun into your server! Splinter has you covered with all sorts of interesting and cool commands to boost user activity and satisfaction!</p></h1>
+              <h1><p style={{fontSize: '14px', margin: '0', padding: '0'}} className="grey">F U N</p><br /><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#29d1ff', color: 'white'}}>😜 All Things Fun!</span><br /><p style={{fontSize: '21px', fontWeight: '600'}}>Looking to bring some fun into your server! Splinter has you covered with all sorts of interesting and cool commands to boost user activity and satisfaction!</p></h1>
             </div>
+
+            <div style={{textAlign: 'center', padding: '40px 10px', boxShadow: '0px 0px 10px rgb(0, 0, 0, 0.15)', borderRadius: '10px', backgroundImage: "url('/imgs/premium-branding.png')", backgroundSize: '50%', border: '2px solid rgb(0, 0, 0, 0.3)'}}>
+              <div className="infocard" style={{width: '100%'}}>
+                <Image
+                  src="/imgs/splinter-pfp.png"
+                  width="100"
+                  height="100"
+                />
+                <h1 style={{color: '#29d1ff', fontWeight: '800', fontSize: '45px'}}><span className="grey">|</span>  Premium</h1>
+              </div>
+              <h2 style={{marginTop: '-30px', padding: '0', backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}>Take your server to the next level with Splinter Premium. Get access to ticketing, levelling, and more premium perks for a low monthly price.</h2>
+              
+          </div>
             
           </div>
 
